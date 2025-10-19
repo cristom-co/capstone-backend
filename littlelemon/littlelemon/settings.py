6b64775695a61254ba099b3e96b0272bb8523e5b
Cristhian Tombe <cristhiantombe@gmail.com>
2025-10-19 14:36:57 -0500

    home page

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -54,17 +54,20 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
