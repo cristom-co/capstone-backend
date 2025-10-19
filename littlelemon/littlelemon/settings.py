e972e8bc298d225a42e79a21d2e3d5dd5c44f97e
Cristhian Tombe <cristhiantombe@gmail.com>
2025-10-19 16:55:31 -0500

    tests

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -133,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#add the following line
+DJOSER={"USER_ID_FIELD":"username"}
